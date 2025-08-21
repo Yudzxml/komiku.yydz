3f14d7a86748d1bb8e2c26ba80425d2daaa56d9b
Yudzxml <yudaaryaardhana1122@gmail.com>
2025-08-21 08:52:21 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "komiku-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html"
+];
+
+// Install service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch dari cache dulu, baru ke network
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
+
+// Update cache lama
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
+        })
+      )
+    )
+  );
+});
